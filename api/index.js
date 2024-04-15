7ec084c8e4c42c38f7524702d9d6ea0d612794a4
Thosam1 <thosamtenzin2002@gmail.com>
2024-04-15 15:51:18 +0200

    trying to see if api works at least

--- api/index.js
+++ api/index.js
@@ -11,6 +11,10 @@ const express = require("express")
 const app = express()
 const port = 4000
 
+app.get('/home', (req, res) => {
+    res.status(200).json('Welcome, your app is working well');
+})  
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
